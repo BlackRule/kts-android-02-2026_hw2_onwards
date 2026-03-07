1bb64f9f5363b0d6af5c5b780d15bfdb98ab7889
BlackRule <rulesprog@gmail.com>
2026-03-07 21:26:42 +0200

    hw4 v2 (enabled https)

--- shared/src/commonMain/kotlin/com/example/myapplication/Constants.kt
+++ shared/src/commonMain/kotlin/com/example/myapplication/Constants.kt
@@ -1,7 +1,6 @@
 package com.example.myapplication
 
-const val SERVER_PORT = 9878
-const val SERVER_BASE_URL = "http://195.46.171.236:$SERVER_PORT"
+const val DEFAULT_SERVER_PORT = 9878
 const val DEFAULT_SHOPS_PAGE_SIZE = 20
 const val DEFAULT_LOGIN_USERNAME = "admin"
 const val DEFAULT_LOGIN_PASSWORD = "admin123"
